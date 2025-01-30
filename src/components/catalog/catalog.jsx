9f4053aa517f4611dd2ec306e11cc6ba99fa5e3f
Miami <alex.miami.petrov@gmail.com>
2025-01-30 11:38:32 +0200

    m

--- src/components/catalog/catalog.jsx
+++ src/components/catalog/catalog.jsx
@@ -5,7 +5,20 @@ import Container from "../../utils/container/container.jsx";
 const Catalog = () => {
   return (
     <section className={s.catalog}>
-      <Container></Container>
+      <Container>
+        <p className={s.catalogText}>Location</p>
+        <form onSubmit={handleSubmit} className={s.form}>
+          <div className={`${s.inputGroup} ${errors.name ? s.errorGroup : ""}`}>
+            <input
+              name="location"
+              value={formData.location}
+              placeholder="City"
+              className={s.input}
+            />
+            <svg className={s.icon} width="24" height="24">
+              <use href={`${icons}#icon-name`} />
+            </svg>
+      </Container>
     </section>
   );
 };
