11e60316d1f980e45b30d309fa3d3b3d1d3fa7c5
Miami <alex.miami.petrov@gmail.com>
2025-01-30 11:54:55 +0200

    m

--- src/components/catalog/catalog.jsx
+++ src/components/catalog/catalog.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import s from "./catalog.module.css";
 import Container from "../../utils/container/container.jsx";
+import icons from "../../img/icons.svg";
 
 const Catalog = () => {
   return (
     <section className={s.catalog}>
       <Container>
         <p className={s.catalogText}>Location</p>
-        <form onSubmit={handleSubmit} className={s.form}>
-          <div className={`${s.inputGroup} ${errors.name ? s.errorGroup : ""}`}>
+        <form className={s.form}>
+          <div className={s.inputWrap}>
             <input
               name="location"
-              value={formData.location}
+              // value="location"
               placeholder="City"
               className={s.input}
             />
-            <svg className={s.icon} width="24" height="24">
-              <use href={`${icons}#icon-name`} />
+            <svg className={s.inputIcon} width="20" height="20">
+              <use href={`${icons}#icon-map`} />
             </svg>
+          </div>
+        </form>
       </Container>
     </section>
   );
