c767656ae576278f3e072071422c39184e3c6261
Miami <alex.miami.petrov@gmail.com>
2025-02-02 00:51:17 +0200

    m

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -1,3 +1,41 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   location: "",
+//   equipment: [],
+//   form: "",
+// };
+
+// const filtersSlice = createSlice({
+//   name: "filters",
+//   initialState,
+//   reducers: {
+//     setLocation(state, action) {
+//       state.location = action.payload;
+//     },
+//     toggleEquipment(state, action) {
+//       const item = action.payload;
+//       state.equipment.includes(item)
+//         ? (state.equipment = state.equipment.filter((eq) => eq !== item))
+//         : state.equipment.push(item);
+//     },
+
+//     setForm(state, action) {
+//       state.form = action.payload;
+//     },
+//     resetFilters(state) {
+//       state.location = "";
+//       state.equipment = [];
+//       state.form = "";
+//     },
+//   },
+// });
+
+// export const { setLocation, toggleEquipment, setForm, resetFilters } =
+//   filtersSlice.actions;
+
+// export default filtersSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -19,13 +57,6 @@ const filtersSlice = createSlice({
         ? (state.equipment = state.equipment.filter((eq) => eq !== item))
         : state.equipment.push(item);
     },
-    // toggleEquipment(state, action) {
-    //   const item = action.payload;
-
-    //   state.equipment = state.equipment.includes(item)
-    //     ? state.equipment.filter((eq) => eq !== item)
-    //     : [...state.equipment, item];
-    // },
     setForm(state, action) {
       state.form = action.payload;
     },
