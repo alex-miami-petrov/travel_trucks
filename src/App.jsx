9bc61640e479fb1d380580a469d606d6b9cc4386
Miami <alex.miami.petrov@gmail.com>
2025-02-03 00:04:38 +0200

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-// import { Route, Routes } from "react-router-dom";
-// import { Layout } from "./components/Layout";
-// import HomePage from "../src/pages/homePage/homePage.jsx";
-// import CamperDetailsPage from "../src/pages/camperDetailsPage/camperDetailsPage.jsx";
-// import CatalogPage from "../pages/catalogPage/catalogPage.jsx";
-
-// import Features from "./components/features/features.jsx";
-// import Reviews from "./components/reviews/reviews.jsx";
-
-// export const App = () => {
-//   return (
-//     <Layout>
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/catalog" element={<CatalogPage />} />
-//         <Route path="/catalog/:id" element={<CamperDetailsPage />}>
-//           <Route path="features" element={<Features />} />
-//           <Route path="reviews" element={<Reviews />} />
-//         </Route>
-//       </Routes>
-//     </Layout>
-//   );
-// };
-
-// export default App;
-
 import React, { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout";
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,5 @@
-// import React from "react";
-
-// import Home from "../../components/home/home.jsx";
-
-// const HomePage = () => {
-//   return (
-//     <>
-//       <Home />
-//     </>
-//   );
-// };
-
-// export default HomePage;
-
 import React from "react";
+
 import Home from "../../components/home/home.jsx";
 
 const HomePage = () => {
