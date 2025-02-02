41be1cb598629a2c6fa00144ff443675f3c91000
Miami <alex.miami.petrov@gmail.com>
2025-02-02 22:55:48 +0200

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout";
-import HomePage from "./pages/homePage/homePage.jsx";
+// import HomePage from "./pages/homePage/homePage.jsx";
 import CamperDetailsPage from "./pages/camperDetailsPage/camperDetailsPage.jsx";
 import CatalogPage from "./pages/catalogPage/catalogPage.jsx";
 
@@ -12,7 +12,7 @@ export const App = () => {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
         <Route path="/catalog" element={<CatalogPage />} />
         <Route path="/catalog/:id" element={<CamperDetailsPage />}>
           <Route path="features" element={<Features />} />
