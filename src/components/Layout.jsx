79b7e40bb251867f5c6079f26913895921310cc5
Miami <alex.miami.petrov@gmail.com>
2025-02-03 18:16:20 +0200

    m

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,14 @@
 import React, { Suspense } from "react";
 import { AppBar } from "./appBar/appBar";
 import { Outlet } from "react-router-dom";
+import Loader from "../utils/loader/loader.jsx";
 
 export const Layout = ({ children }) => {
   return (
     <div style={{ maxWidth: 1440, margin: "0 auto", padding: "0 16px" }}>
       <AppBar />
       <Outlet />
-      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      <Suspense fallback={<Loader />}>{children}</Suspense>
     </div>
   );
 };
---
+++ src/utils/loader/loader.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import s from "./loader.module.css";
+
+const Loader = () => <div className={s.loader}></div>;
+
+export default Loader;
